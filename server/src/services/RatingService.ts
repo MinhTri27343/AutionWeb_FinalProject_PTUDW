890de83cc8aa0bfad825671ed6424b6097ad3c8b
Donavfulish <dvha23@clc.fitus.edu.vn>
2025-11-30 20:03:57 +0700

    feat(server): update getRating api

--- server/src/services/RatingService.ts
+++ server/src/services/RatingService.ts
@@ -1,5 +1,6 @@
 import { BaseService } from "./BaseService";
 import { UserRating } from "../../../shared/src/types";
+import { UserRatingHistory } from "../../../shared/src/types";
 
 interface CreateRatingPayload extends UserRating {
     rater_id: string
@@ -21,22 +22,49 @@ export class RatingService extends BaseService {
         return RatingService.instance
     }
 
-    async getRating(userId: number) {
-        const sql =
-            `
-                SELECT sum(rating)
-                FROM feedback.user_ratings
-                WHERE ratee_id = $1
-                `
+    async getRating(userId: number): Promise<UserRatingHistory> {
+        const sql = `
+        SELECT 
+            fur.id as rating_id, fur.rating, fur.comment, fur.created_at, fur.updated_at,
+            aurt.id as rater_id, aurt.name as rater_name, aurt.profile_img as rater_img,
+            aurtt.id as ratee_id, aurtt.name as ratee_name, aurtt.profile_img as ratee_img
+        FROM feedback.user_ratings fur
+        JOIN admin.users aurt ON fur.rater_id = aurt.id
+        JOIN admin.users aurtt ON fur.ratee_id = aurtt.id
+        WHERE fur.ratee_id = $1
+    `
         const params = [userId]
+        const rows = await this.safeQuery(sql, params)
 
-        return await this.safeQuery(sql, params);
+        const ratingHistory: UserRatingHistory = {
+            ratee_id: userId,
+            logs: rows.map((row: any) => ({
+                id: row.rating_id,
+                rater: {
+                    id: row.rater_id,
+                    name: row.rater_name,
+                    profile_img: row.rater_img
+                },
+                ratee: {
+                    id: row.ratee_id,
+                    name: row.ratee_name,
+                    profile_img: row.ratee_img
+                },
+                rating: row.rating,
+                comment: row.comment,
+                created_at: row.created_at,
+                updated_at: row.updated_at || null
+            }))
+        }
+
+        return ratingHistory
     }
 
+
     async createRating(payload: CreateRatingPayload) {
-        const {rater_id, ratee_id, comment, rating} = payload;
-        const sql = 
-                `
+        const { rater_id, ratee_id, comment, rating } = payload;
+        const sql =
+            `
                 INSERT INTO feedback.user_ratings (rater_id, ratee_id, comment, rating)
                 VALUES ( $1, $2, $3, $4 )
                 `
