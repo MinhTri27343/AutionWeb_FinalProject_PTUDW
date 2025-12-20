3db46499605366600f027bb49ca9a8187cb025d1
minhluan <luanthuco11@gmail.com>
2025-12-20 12:27:22 +0700

    fix: hightlight

--- client/components/ProductCard/index.tsx
+++ client/components/ProductCard/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FavoriteButton from "../FavoriteButton";
 import { ProductPreview } from "../../../shared/src/types";
 import { getTimeDifference } from "@/utils";
@@ -9,6 +9,9 @@ import Image from "next/image";
 import FavoriteHook from "@/hooks/useFavorite";
 import LoadingSpinner from "../LoadingSpinner";
 import { formatCurrency } from "@/app/(MainLayout)/product/[product_slug]/components/Question";
+import SystemHook from "@/hooks/useSystem";
+import { boolean } from "zod";
+import { fa } from "zod/v4/locales";
 
 const defaultImage =
   "https://img.freepik.com/premium-photo/white-colors-podium-abstract-background-minimal-geometric-shape-3d-rendering_48946-113.jpg?semt=ais_hybrid&w=740&q=80";
@@ -25,6 +28,30 @@ export default function ProductCard({
   const { mutate: removeFavorite, isPending: isRemoving } =
     FavoriteHook.useRemoveFavorite();
 
+  const [isHighlight, setIsHighlight] = useState<boolean>(false);
+  /// Min Time
+  const {
+    data: serverMinTime,
+    isLoading: isLoadingMinTime,
+    isError: isErrorMinTime,
+  } = SystemHook.useGetProductMinTime();
+  let minTime;
+  useEffect(() => {
+    if (serverMinTime) {
+      minTime = serverMinTime[0].new_product_min_time;
+
+      const nowTime = new Date();
+      const createProductTime = new Date(product.created_at);
+
+      const diffM = (nowTime.getTime() - createProductTime.getTime()) / 60000;
+      if (diffM <= minTime) {
+        setIsHighlight(true);
+      } else {
+        setIsHighlight(false);
+      }
+    }
+  }, [serverMinTime]);
+  ///
   const handleFavorite = (productId: number, isFavorite: boolean) => {
     try {
       if (isFavorite) {
@@ -38,109 +65,128 @@ export default function ProductCard({
   };
 
   return (
-    <div className="group relative w-full h-fit pb-3 rounded-lg border-2 border-gray-200 bg-white shadow-md hover:shadow-2xl hover:border-blue-500 transition-all duration-200 select-none">
-      <Image
-        src={product.main_image || defaultImage}
-        width={50}
-        height={123}
-        alt={product.name}
-        className="w-full aspect-5/4 rounded-t-md"
-      />
-      <Link href={`/product/${product.slug}`} className="block px-3">
-        <section className="mt-2">
-          <div className="h-18">
-            <p className="font-medium max-h-12 line-clamp-2">{product.name}</p>
-            <div className="mt-1 flex flex-row gap-1">
+    <div
+      className={`relative border border-red-500 rounded-lg shadow-md hover:shadow-2xl`}
+    >
+      <div className="absolute z-10">
+        <svg
+          className="w-6 h-6 text-red-500 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width={24}
+          height={24}
+          fill="currentColor"
+          viewBox="0 0 24 24"
+        >
+          <path d="M8.597 3.2A1 1 0 0 0 7.04 4.289a3.49 3.49 0 0 1 .057 1.795 3.448 3.448 0 0 1-.84 1.575.999.999 0 0 0-.077.094c-.596.817-3.96 5.6-.941 10.762l.03.049a7.73 7.73 0 0 0 2.917 2.602 7.617 7.617 0 0 0 3.772.829 8.06 8.06 0 0 0 3.986-.975 8.185 8.185 0 0 0 3.04-2.864c1.301-2.2 1.184-4.556.588-6.441-.583-1.848-1.68-3.414-2.607-4.102a1 1 0 0 0-1.594.757c-.067 1.431-.363 2.551-.794 3.431-.222-2.407-1.127-4.196-2.224-5.524-1.147-1.39-2.564-2.3-3.323-2.788a8.487 8.487 0 0 1-.432-.287Z" />
+        </svg>
+      </div>
+      <div className="group relative w-full h-fit pb-3 rounded-lg border-2 border-gray-200 bg-white  hover:border-blue-500 transition-all duration-200 select-none">
+        <Image
+          src={product.main_image || defaultImage}
+          width={50}
+          height={123}
+          alt={product.name}
+          className="w-full aspect-5/4 rounded-t-md"
+        />
+        <Link href={`/product/${product.slug}`} className="block px-3">
+          <section className="mt-2">
+            <div className="h-18">
+              <p className="font-medium max-h-12 line-clamp-2">
+                {product.name}
+              </p>
+              <div className="mt-1 flex flex-row gap-1">
+                <svg
+                  className="w-6 h-6 text-red-500 dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={24}
+                  height={24}
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M8.597 3.2A1 1 0 0 0 7.04 4.289a3.49 3.49 0 0 1 .057 1.795 3.448 3.448 0 0 1-.84 1.575.999.999 0 0 0-.077.094c-.596.817-3.96 5.6-.941 10.762l.03.049a7.73 7.73 0 0 0 2.917 2.602 7.617 7.617 0 0 0 3.772.829 8.06 8.06 0 0 0 3.986-.975 8.185 8.185 0 0 0 3.04-2.864c1.301-2.2 1.184-4.556.588-6.441-.583-1.848-1.68-3.414-2.607-4.102a1 1 0 0 0-1.594.757c-.067 1.431-.363 2.551-.794 3.431-.222-2.407-1.127-4.196-2.224-5.524-1.147-1.39-2.564-2.3-3.323-2.788a8.487 8.487 0 0 1-.432-.287Z" />
+                </svg>
+
+                <span>{product.bid_count} lượt đấu giá</span>
+              </div>
+            </div>
+            <div className="mt-3">
+              <p className="text-sm">Giá hiện tại</p>
+              <p>
+                <span className="text-2xl font-medium text-blue-600">
+                  {formatCurrency(product.current_price)}
+                </span>
+              </p>
+            </div>
+            <div className="mt-1">
+              <p className="text-sm">Giá mua ngay</p>
+              <p>
+                <span className="text-xl font-medium text-red-500">
+                  {product.buy_now_price
+                    ? formatCurrency(product.buy_now_price)
+                    : "---"}
+                </span>
+              </p>
+            </div>
+
+            <div className="mt-3 h-10">
+              {product.top_bidder_name ? (
+                <div>
+                  <p className="text-sm">Người trả giá cao nhất</p>
+                  <p className="font-medium">{product.top_bidder_name[0]}***</p>
+                </div>
+              ) : (
+                <div>
+                  <p className="text-sm text-gray-800">Chưa có lượt trả giá</p>
+                </div>
+              )}
+            </div>
+          </section>
+          <hr className="border-t border-solid border-gray-300 mt-3 mb-1.5" />
+          <section className="flex flex-col gap-1.5">
+            <p className="text-sm text-gray-500">
+              Ngày bắt đầu:{" "}
+              {new Date(product.created_at).toLocaleDateString("en-GB")}
+            </p>
+            <div className="flex flex-row gap-2 items-center">
               <svg
-                className="w-6 h-6 text-red-500 dark:text-white"
+                className="w-6 h-6 text-gray-800 dark:text-white"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 width={24}
                 height={24}
-                fill="currentColor"
+                fill="none"
                 viewBox="0 0 24 24"
               >
-                <path d="M8.597 3.2A1 1 0 0 0 7.04 4.289a3.49 3.49 0 0 1 .057 1.795 3.448 3.448 0 0 1-.84 1.575.999.999 0 0 0-.077.094c-.596.817-3.96 5.6-.941 10.762l.03.049a7.73 7.73 0 0 0 2.917 2.602 7.617 7.617 0 0 0 3.772.829 8.06 8.06 0 0 0 3.986-.975 8.185 8.185 0 0 0 3.04-2.864c1.301-2.2 1.184-4.556.588-6.441-.583-1.848-1.68-3.414-2.607-4.102a1 1 0 0 0-1.594.757c-.067 1.431-.363 2.551-.794 3.431-.222-2.407-1.127-4.196-2.224-5.524-1.147-1.39-2.564-2.3-3.323-2.788a8.487 8.487 0 0 1-.432-.287Z" />
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 8v4l3 3m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
               </svg>
-
-              <span>{product.bid_count} lượt đấu giá</span>
+              <span>
+                {getTimeDifference(new Date(), new Date(product.end_time))}
+              </span>
             </div>
-          </div>
-          <div className="mt-3">
-            <p className="text-sm">Giá hiện tại</p>
-            <p>
-              <span className="text-2xl font-medium text-blue-600">
-                {formatCurrency(product.current_price)}
-              </span>
-            </p>
-          </div>
-          <div className="mt-1">
-            <p className="text-sm">Giá mua ngay</p>
-            <p>
-              <span className="text-xl font-medium text-red-500">
-                {product.buy_now_price
-                  ? formatCurrency(product.buy_now_price)
-                  : "---"}
-              </span>
-            </p>
-          </div>
+          </section>
+        </Link>
 
-          <div className="mt-3 h-10">
-            {product.top_bidder_name ? (
-              <div>
-                <p className="text-sm">Người trả giá cao nhất</p>
-                <p className="font-medium">{product.top_bidder_name[0]}***</p>
-              </div>
-            ) : (
-              <div>
-                <p className="text-sm text-gray-800">Chưa có lượt trả giá</p>
-              </div>
-            )}
-          </div>
-        </section>
-        <hr className="border-t border-solid border-gray-300 mt-3 mb-1.5" />
-        <section className="flex flex-col gap-1.5">
-          <p className="text-sm text-gray-500">
-            Ngày bắt đầu:{" "}
-            {new Date(product.created_at).toLocaleDateString("en-GB")}
-          </p>
-          <div className="flex flex-row gap-2 items-center">
-            <svg
-              className="w-6 h-6 text-gray-800 dark:text-white"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              width={24}
-              height={24}
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M12 8v4l3 3m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-              />
-            </svg>
-            <span>
-              {getTimeDifference(new Date(), new Date(product.end_time))}
-            </span>
-          </div>
-        </section>
-      </Link>
-
-      {/* Favourite Button */}
-      <div
-        className={`absolute top-1.5 left-1.5 opacity-0 group-hover:opacity-100 transition-opacity duration-200 ${
-          (isAdding || isRemoving) && "pointer-events-none"
-        }`}
-      >
-        <FavoriteButton
-          isFavorite={isFavorite}
-          onClick={() => handleFavorite(product.id, !isFavorite)}
-        />
+        {/* Favourite Button */}
+        <div
+          className={`absolute top-1.5 left-1.5 opacity-0 group-hover:opacity-100 transition-opacity duration-200 ${
+            (isAdding || isRemoving) && "pointer-events-none"
+          }`}
+        >
+          <FavoriteButton
+            isFavorite={isFavorite}
+            onClick={() => handleFavorite(product.id, !isFavorite)}
+          />
+        </div>
+        {(isAdding || isRemoving) && <LoadingSpinner />}
       </div>
-      {(isAdding || isRemoving) && <LoadingSpinner />}
     </div>
   );
 }
