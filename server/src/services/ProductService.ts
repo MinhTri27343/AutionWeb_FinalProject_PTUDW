006c0883e23d2ab3325f5471adadc8dd02f0be9d
minhluan <luanthuco11@gmail.com>
2026-01-05 08:49:22 +0700

    fix: sendEmail when edit product

--- server/src/services/ProductService.ts
+++ server/src/services/ProductService.ts
@@ -923,6 +923,7 @@ RETURNING *;
 
     const sql =
       "DELETE FROM product.products WHERE id = $1 RETURNING MAIN_IMAGE, EXTRA_IMAGES";
+
     const result = await this.safeQuery<{
       main_image: string;
       extra_images: string[];
